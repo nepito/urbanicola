0d52eb8b1133b62b6fdf9103be1220ad263fa339
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-11-10 20:42:19 -0800

    ⚗️ Try first post

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,11 @@ import json
 import pandas as pd
 import streamlit as st
 
+url = "http://104.248.109.197:6868/v1/item"
+user = {"name": ["Primo Rivas"], "age": [38], "client": [False]}
+
+if st.button("Make post request"):
+    st.write(requests.post(url, user))
 
 conn = requests.get("http://104.248.109.197:6868/v1/gastos")
 gastos = pd.DataFrame.from_dict(json.loads(conn.json()))
