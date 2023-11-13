98abcf97af061dc2eaa97c6b5010ecb3895d26d0
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-11-12 19:59:41 -0800

    🎨 Format with Black

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,19 @@ import pandas as pd
 import streamlit as st
 
 name = st.text_input(
-        "Por favor agrega un nombre",
-        placeholder="Nombre",
-    )
-age = st.number_input('Agrega una edad por favor', min_value=18, max_value=65)
-client = st.checkbox('Cliente')
+    "Por favor agrega un nombre",
+    placeholder="Nombre",
+)
+age = st.number_input("Agrega una edad por favor", min_value=18, max_value=65)
+client = st.checkbox("Cliente")
 url = "http://104.248.109.197:6868/v1/item"
+
+
 def make_user(nombre, edad, es_cliente):
     return {"name": [nombre], "age": [edad], "client": [es_cliente]}
-headers = {'Content-type': 'application/json'}
+
+
+headers = {"Content-type": "application/json"}
 
 if st.button("Make post request"):
     st.write(requests.post(url, json=make_user(name, age, client), headers=headers))
